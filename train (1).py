ac327380a10515f08ae88b1a3c55383a8f5f3bfb
tas12saini <62707000+tas12saini@users.noreply.github.com>
2020-03-27 13:44:32 +0530

    Add files via upload

---
+++ train (1).py
@@ -0,0 +1,142 @@
+import numpy as np
+from keras import layers
+from keras.layers import Input, Dense, Activation, ZeroPadding2D, BatchNormalization, Flatten, Conv2D
+from keras.layers import AveragePooling2D, MaxPooling2D, Dropout, GlobalMaxPooling2D, GlobalAveragePooling2D
+from keras.models import Model
+from keras.models import Sequential
+from keras.preprocessing import image
+from keras.utils import layer_utils
+from keras.utils.data_utils import get_file
+from keras.applications.imagenet_utils import preprocess_input
+import pydot
+from IPython.display import SVG
+from keras.utils.vis_utils import model_to_dot
+from keras.utils import plot_model
+from keras.utils.np_utils import to_categorical
+#from kt_utils import *
+
+import keras.backend as K
+K.set_image_data_format('channels_last')
+import matplotlib.pyplot as plt
+from matplotlib.pyplot import imshow
+import pickle
+
+import numpy as np
+import os
+#import matplotlib.pyplot as plt
+import cv2
+import random
+import pickle
+
+file_list = []
+class_list = []
+DATADIR = "Data"
+
+CATEGORIES = ["class0", "class1"]
+
+# The size of the images that your neural network will use
+IMG_SIZE = 300
+
+for category in CATEGORIES :
+    path = os.path.join(DATADIR, category)
+    for img in os.listdir(path):
+        img_array = cv2.imread(os.path.join(path, img), cv2.IMREAD_GRAYSCALE)
+
+training_data = []
+def create_training_data():
+    for category in CATEGORIES :
+        path = os.path.join(DATADIR, category)
+        class_num = CATEGORIES.index(category)
+        for img in os.listdir(path):
+            try :
+                img_array = cv2.imread(os.path.join(path, img), cv2.IMREAD_GRAYSCALE)
+                new_array = cv2.resize(img_array, (IMG_SIZE, IMG_SIZE))
+                training_data.append([new_array, class_num])
+            except Exception as e:
+                pass
+
+create_training_data()
+
+random.shuffle(training_data)
+X = [] #features
+y = [] #labels
+
+for features, label in training_data:
+    X.append(features)
+    y.append(label)
+
+X = np.array(X).reshape(-1, IMG_SIZE, IMG_SIZE, 1)
+pickle_out = open("X.pickle", "wb")
+pickle.dump(X, pickle_out)
+pickle_out.close()
+
+pickle_out = open("y.pickle", "wb")
+pickle.dump(y, pickle_out)
+pickle_out.close()
+
+pickle_in = open("X.pickle", "rb")
+X = pickle.load(pickle_in)
+
+X = pickle.load(open("X.pickle", "rb"))
+y = pickle.load(open("y.pickle", "rb"))
+
+# normalizing data (a pixel goes from 0 to 255)
+X = X/255.0
+
+# Building the model
+model = Sequential()
+# 3 convolutional layers
+model.add(Conv2D(32, (3, 3), input_shape = X.shape[1:]))
+model.add(Activation("relu"))
+model.add(MaxPooling2D(pool_size=(2,2)))
+
+model.add(Conv2D(64, (3, 3)))
+model.add(Activation("relu"))
+model.add(MaxPooling2D(pool_size=(2,2)))
+
+model.add(Conv2D(64, (3, 3)))
+model.add(Activation("relu"))
+model.add(MaxPooling2D(pool_size=(2,2)))
+model.add(Dropout(0.25))
+
+# 2 hidden layers  
+model.add(Flatten())
+model.add(Dense(128))
+model.add(Activation("relu"))
+
+model.add(Dense(128))
+model.add(Activation("relu"))
+
+# The output layer
+model.add(Dense(2))
+model.add(Activation("sigmoid"))
+
+# Compiling the model using some basic parameters
+model.compile(loss="sparse_categorical_crossentropy",
+				optimizer="adam",
+				metrics=["accuracy"])
+
+# Training the model
+# validation_split corresponds to the percentage of images used for the validation phase compared to all the images
+history = model.fit(X, y, batch_size=30, epochs=3, validation_split=0.1)
+
+# Saving the model
+model_json = model.to_json()
+with open("model.json", "w") as json_file :
+	json_file.write(model_json)
+
+model.save_weights("savedweights.h5")
+print("Saved model to disk")
+
+model.save('CNN.model')
+
+#Printing a graph showing the accuracy changes during the training phase
+print(history.history.keys())
+plt.figure(1)
+plt.plot(history.history['accuracy'])
+plt.plot(history.history['val_accuracy'])
+plt.title('model accuracy')
+plt.ylabel('accuracy')
+plt.xlabel('epoch')
+plt.legend(['train', 'validation'], loc='upper left')
+plt.show()
